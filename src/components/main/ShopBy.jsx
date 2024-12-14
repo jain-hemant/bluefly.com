77b21408c2445c7f410585f432f273ba49a70512
jain-hemant <hemantjain1321@gmail.comcls>
2024-12-15 00:41:54 +0530

    SHOP BY card UI desined well

--- src/components/main/ShopBy.jsx
+++ src/components/main/ShopBy.jsx
@@ -5,25 +5,25 @@ import { Link } from 'react-router'
 
 
 const ShopBy = () => {
-  return (
-    <Flex  flexWrap={"wrap"} gap={"10px"} boxSizing={"border-box"}>
-        {
-            shopByCategory.map((product)=>{
-                return <Box key={product.id} w={["100%","100%","24.4%"]} >
-                    <Box>
-                        <Image src={product.img}/>
+    return (
+        <Flex flexWrap={"wrap"} gap={"10px"} boxSizing={"border-box"}>
+            {
+                shopByCategory.map((product) => {
+                    return <Box key={product.id} w={["100%", "100%", "24.4%"]} >
+                        <Box>
+                            <Image src={product.img} borderTopRadius={"5px"} />
+                        </Box>
+                        <Box py={"7px"} textAlign={"center"} bg={"purple.fg"} color={"white"} borderBottomRadius={"5px"}>
+                            <Heading color={"goldenrod"}>{product.category}</Heading>
+                            <Heading color={"silver"}>{product.productType}</Heading>
+                            <Box fontSize={"12px"} textDecoration={"underline"}><Link>SHOP NOW </Link></Box>
+                        </Box>
                     </Box>
-                    <Box textAlign={"center"}>
-                         <Heading>{product.category}</Heading>
-                        <Heading>{product.productType}</Heading>
-                        <Link>SHOP NOW </Link>
-                    </Box> 
-                </Box>
-            })
-        }
+                })
+            }
 
-    </Flex>
-  )
+        </Flex>
+    )
 }
 
 export default ShopBy
